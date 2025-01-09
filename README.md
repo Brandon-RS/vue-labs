38d335708881caef1573636a2ae2cd38687f6354
Brandom <brandon.rsantillan@proton.me>
2025-01-08 23:58:57 -0500

    feat: Add `brs` web app

---
+++ README.md
@@ -0,0 +1,8 @@
+# Vue monorepo
+
+A place to host some vue apps.
+
+## Apps
+
+- [Main web app](./brs/)
+- [Personal portfolio](./brandon/)
---
+++ brs/src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <h1>Brandon pub</h1>
+
+    <ul>
+      <li>
+        <a href="https://brandon.brs.pub" target="_blank">Get to know me!</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped></style>

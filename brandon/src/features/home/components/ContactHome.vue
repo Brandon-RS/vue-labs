bd761371d11fac481e906bae992827ba36dd50ee
Brandom <brandon.rsantillan@proton.me>
2025-01-16 23:14:11 -0500

    feat: Home contact section

---
+++ brandon/src/features/home/components/ContactHome.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="flex gap-5 mt-8 justify-center md:justify-start">
+    <button
+      class="px-5 py-2 rounded-full bg-amber-300 dark:bg-amber-400 text-primary">
+      <router-link :to="{ name: 'contact' }">Let's talk</router-link>
+    </button>
+
+    <button
+      class="px-5 py-2 rounded-full bg-gray-200 dark:bg-gray-600 border-2 dark:border-gray-600 hover:border-gray-400">
+      <router-link :to="{ name: 'projects' }">Check my work</router-link>
+    </button>
+  </div>
+</template>
+
+<style scoped></style>
--- brandon/src/features/home/views/HomeView.vue
+++ brandon/src/features/home/views/HomeView.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
-import BannerHome from '@/features/home/components/BannerHome.vue'
+import BannerHome from '@/features/home/components/BannerHome.vue';
+import ContactHome from '@/features/home/components/ContactHome.vue';
+import ProjectsHome from '@/features/home/components/ProjectsHome.vue';
 </script>
 
 <template>
   <section class="pt-5 md:pt-20">
     <banner-home name="Brandom" />
+
+    <contact-home />
+
+    <projects-home />
   </section>
 </template>
 

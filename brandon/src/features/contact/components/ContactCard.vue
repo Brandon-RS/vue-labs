7b0e0aa839faa58408624c03d4fc0fab423ce267
Brandom <brandon.rsantillan@proton.me>
2025-05-18 23:02:46 -0500

    feat: Add contact section

---
+++ brandon/src/features/contact/components/ContactCard.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+
+defineProps<{
+  title: string;
+  text: string;
+  contact: string;
+  link: string;
+  icon: string;
+}>();
+
+</script>
+
+<template>
+  <div class="bg-gray-200 dark:bg-slate-800 rounded-xl p-5 cursor-pointer">
+
+    <img
+      class="w-16 h-16 mx-auto mb-4 icon-grey"
+      :src=icon :alt=title>
+
+    <h4 class="text-lg text-zinc-700 dark:text-zinc-200 font-semibold">
+      {{ title }}
+    </h4>
+
+    <hr class="border-gray-700 border-2 rounded-full my-4" />
+
+    <p class="text-sm text-gray-600 dark:text-gray-300 mt-2">
+      {{ text }}
+    </p>
+
+    <p class="text-sm text-gray-600 dark:text-gray-300 mt-2 mb-4">
+      {{ contact }}
+    </p>
+
+    <a :href="link" target="_blank" rel="noopener noreferrer">
+      <button
+        class="w-full mt-6 px-5 py-2 rounded-lg border-gray-700 dark:border-amber-400 border-2 text-zinc-700 dark:text-zinc-200 hover:bg-gray-300 dark:hover:bg-slate-700 hover:text-zinc-900 dark:hover:text-zinc-200 transition duration-200 ease-in-out">
+        Let's chat!
+      </button>
+    </a>
+  </div>
+</template>
+
+<style scoped>
+.dark .icon-grey {
+  filter: brightness(0) saturate(100%) invert(90%) sepia(0%) saturate(0%) hue-rotate(0deg) brightness(95%) contrast(90%);
+}
+</style>
--- brandon/src/features/contact/views/ContactView.vue
+++ brandon/src/features/contact/views/ContactView.vue
@@ -1,9 +1,49 @@
 <script setup lang="ts">
+import { useHomeProjectsStore } from '@home/stores/useHomeProjectsStore';
+import { useRouter } from 'vue-router';
+import ContactCard from '../components/ContactCard.vue';
+
+const router = useRouter();
+const projectsStore = useHomeProjectsStore();
+
+
 </script>
 
 <template>
-  <div>
-    <h2>Whe are still working on this ...</h2>
+  <div class="mt-10 md:mt-12">
+
+    <h2 class="text-4xl text-center pb-8 max-w-sm mx-auto">
+      Get in touch for more information
+    </h2>
+
+    <h3 class="text-lg text-center">
+      If you need help or have any questions, let's have a chat!
+    </h3>
+
+    <div class="pt-16 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+
+      <ContactCard
+        title="LinkedIn"
+        text="Feel free to connect with me on LinkedIn."
+        contact="Brandon Rojas (brandon-rs)"
+        link="https://www.linkedin.com/in/brandon-rs/"
+        icon="https://cdn.jsdelivr.net/npm/simple-icons@v6/icons/linkedin.svg" />
+
+      <ContactCard
+        title="GitHub"
+        text="Check out my GitHub profile."
+        contact="Brandon Rojas"
+        link="https://github.com/Brandon-RS"
+        icon="https://cdn.jsdelivr.net/npm/simple-icons@v6/icons/github.svg" />
+
+      <ContactCard
+        title="Email"
+        text="If you prefer, you can also reach out via email."
+        contact="brandon.rsantillan@proton.me"
+        link="mailto:brandon.rsantillan@proton.me"
+        icon="https://cdn.jsdelivr.net/npm/simple-icons@v6/icons/protonmail.svg" />
+
+    </div>
   </div>
 </template>
 
